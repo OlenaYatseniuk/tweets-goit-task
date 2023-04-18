bcb7654935f366d771d66e6108e51eae2a92673c
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-04-18 21:02:16 +0300

    Updates

---
+++ static/js/156.349d2657.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktweets_goit_task=self.webpackChunktweets_goit_task||[]).push([[156],{156:function(e,s,t){t.r(s),t.d(s,{default:function(){return r}});var a=t(87),n="HomePage_homeSection__unkVc",c="HomePage_wrapper__fn5mj",i="HomePage_link__LGUxJ",o=t(184);var r=function(){return(0,o.jsxs)("section",{className:n,children:[(0,o.jsx)("h1",{className:c,children:"This is HomePage! I have no fantasy to create something special =)"}),(0,o.jsxs)(a.rU,{to:"/tweets",className:i,children:["See all users"," "]})]})}}}]);
+//# sourceMappingURL=156.349d2657.chunk.js.map

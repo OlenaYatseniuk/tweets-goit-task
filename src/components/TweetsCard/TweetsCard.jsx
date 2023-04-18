0b70a1b2371f602cdf134ea7ffc5d3b80ae78f37
Olena Yatsenyuk <elena.yatsenyuk@gmail.com>
2023-04-18 16:30:49 +0300

    add css to TweetsCard

--- src/components/TweetsCard/TweetsCard.jsx
+++ src/components/TweetsCard/TweetsCard.jsx
@@ -1,34 +1,33 @@
 import s from "./TweetsCard.module.scss";
-import picture1x from '../../assets/pictures/picture2-1x.png';
-import picture2x from '../../assets/pictures/picture2-2x.png';
-import logo1x from '../../assets/pictures/logo-1x.png';
-import logo2x from '../../assets/pictures/logo-2x.png';
-
-
-
-
+import picture1x from "../../assets/pictures/picture2-1x.png";
+import picture2x from "../../assets/pictures/picture2-2x.png";
+import logo1x from "../../assets/pictures/logo-1x.png";
+import logo2x from "../../assets/pictures/logo-2x.png";
 
 function TweetsCard() {
   return (
-      <li className={s.tweets__container}>
-          <picture>
-              <source srcSet={`${picture1x} 1x, ${picture2x} 2x`} />
-              <img src={picture1x } alt="decoration background"/>
-          </picture>
+    <li className={s.tweets__container}>
+      <picture>
+        <source srcSet={`${picture1x} 1x, ${picture2x} 2x`} />
+        <img src={picture1x} alt="decoration background" />
+      </picture>
 
-          <picture>
-              <source srcSet={`${logo1x} 1x, ${logo2x} 2x`} />
-              <img src={logo1x } alt="logo"/>
-          </picture>
-          <div className={s.tweets__line } />
+      <picture className={s.tweets__logo}>
+        <source srcSet={`${logo1x} 1x, ${logo2x} 2x`} />
+        <img src={logo1x} alt="logo" />
+      </picture>
+      <div className={s.tweets__line} />
+      <div className={s.tweets__avatarWrapper} />
       <img
         className={s.tweets__avatar}
         alt="user avatar"
-        src="../../assets/pictures/avatars/avatar1.png"
+        src="../../assets/pictures/avatars/avatar2.png"
       />
-      <p className={s.tweets__tweetsNumber}>777 Tweets</p>
+      <p className={s.tweets__tweetsText}>777 Tweets</p>
       <p className={s.tweets__followers}>100,500 followers</p>
-      <button type="button" className={s.tweets_button}>Follow</button>
+      <button type="button" className={s.tweets_button}>
+        Follow
+      </button>
     </li>
   );
 }
